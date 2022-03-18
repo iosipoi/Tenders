39a0c64e90c2196a3579ffa4390d3cc5fdfa14fa
iosip <iosipoi@gmail.com>
2022-03-18 19:28:02 +0200

    functional tender first row added

---
+++ Tender/Data/TenderContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Tender.Models;
+
+namespace Tender.Data
+{
+    public class TenderContext : DbContext
+    {
+        public TenderContext (DbContextOptions<TenderContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tender.Models.TenderDetails> Tender { get; set; }
+    }
+}
